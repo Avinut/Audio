fb2150d68679773f2905e865797f69fe9bc17d0b
Avinut <77930395+Avinut@users.noreply.github.com>
2022-12-14 18:52:11 +0530

    Update sr.py

--- sr.py
+++ sr.py
@@ -40,44 +40,4 @@ st.markdown('''<style>.css-nlntq9 a {color: #ff4c4b;}</style>''',
     unsafe_allow_html=True)  # lightmode
 
 
-def audiorec_demo_app():
-
-    parent_dir = os.path.dirname(os.path.abspath(_file_))
-    # Custom REACT-based component for recording client audio in browser
-    build_dir = os.path.join(parent_dir, "st_audiorec/frontend/build")
-    # specify directory and initialize st_audiorec object functionality
-    st_audiorec = components.declare_component("st_audiorec", path=build_dir)
-
-    # TITLE and Creator information
-    st.title('streamlit audio recorder')
-    st.markdown('Implemented by '
-        '[Stefan Rummer](https://www.linkedin.com/in/stefanrmmr/) - '
-        'view project source code on '
-        '[GitHub](https://github.com/stefanrmmr/streamlit_audio_recorder)')
-    st.write('\n\n')
-
-
-    # STREAMLIT AUDIO RECORDER Instance
-    val = st_audiorec()
-    # web component returns arraybuffer from WAV-blob
-    st.write('Audio data received in the Python backend will appear below this message ...')
-
-    if isinstance(val, dict):  # retrieve audio data
-        with st.spinner('retrieving audio-recording...'):
-            ind, val = zip(*val['arr'].items())
-            ind = np.array(ind, dtype=int)  # convert to np array
-            val = np.array(val)             # convert to np array
-            sorted_ints = val[ind]
-            stream = BytesIO(b"".join([int(v).to_bytes(1, "big") for v in sorted_ints]))
-            wav_bytes = stream.read()
-
-        # wav_bytes contains audio data in format to be further processed
-        # display audio data as received on the Python side
-        st.audio(wav_bytes, format='audio/wav')
-
-
-if __name__=='__main__':
-
-    # call main function
-    audiorec_demo_app()
 
